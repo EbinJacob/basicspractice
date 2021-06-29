9e0ef24d20d260fc5cf6a3a8d51177aaddbe2ad1
unknown <ebinjacob1999@gmail.com>
2021-06-29 19:09:24 +0530

    stack complete

--- stack.py
+++ stack.py
@@ -6,4 +6,23 @@ class stack:
     def push(self, val):
         self.items.append(val)
     def pop(self):
-        self
+        return self.items.pop()
+
+s = stack()
+while True:
+    print("1. Push")
+    print("2. Pop")
+    print("3. Quit")
+    ch = int(input("Enter your choice: "))
+    if ch==1:
+        val = int(input("Enter value to be pushed: "))
+        s.push(val)
+    elif ch==2:
+        if s.isempty():
+            print("Stack is empty")
+        else:
+            print("Popped value is: ", s.pop())
+    elif ch==3:
+        break
+    else:
+        print("Enter a valid choice!")
