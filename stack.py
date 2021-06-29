a2d8502eb1d6fb8f5bd9d1d030aa0b17eaef5811
unknown <ebinjacob1999@gmail.com>
2021-06-29 18:58:40 +0530

    stackupdate

--- stack.py
+++ stack.py
@@ -2,4 +2,8 @@ class stack:
     def __init__(self):
         self.items=[]
     def isempty(self):
-         
+        return self.items == []
+    def push(self, val):
+        self.items.append(val)
+    def pop(self):
+        self
